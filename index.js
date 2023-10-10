f157848fe99d1f57bfe5f52ce0c8b9781c2b42d7
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-10-10 12:05:34 -0500

    tests passing

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+class Cat {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+      this.name = name;
+      this.sex = sex;
+    }
+  
+    speak() {
+      if (this.sex === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Example usages:
+  const fluffy = new Cat("Fluffy", "female");
+  console.log(fluffy.speak()); // "Fluffy says meow!"
+  
+  const fido = new Dog("Fido", "male");
+  console.log(fido.speak()); // "Fido says woof!"
+  
+  const polly = new Bird("Polly", "male");
+  console.log(polly.speak()); // "It's me! Polly, the parrot!"
+  
+  const tweety = new Bird("Tweety", "female");
+  console.log(tweety.speak()); // "Tweety says squawk!"
+  
